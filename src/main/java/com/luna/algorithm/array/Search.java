e70ce097cb405af595e84a5af7f5770e9ec35099
luna <449310419@qq.com>
2021-02-22 10:28:15 +0800

    add Search test

--- src/main/java/com/luna/algorithm/array/Search.java
+++ src/main/java/com/luna/algorithm/array/Search.java
@@ -30,6 +30,49 @@ package com.luna.algorithm.array;
 
 public class Search {
 
+    public int searchTest(int[] nums, int target){
+        if(nums.length == 0){
+            return -1;
+        }
+        if(nums.length == 1){
+            return nums[0] == target? 0:-1;
+        }
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+           int mid = (left+right)/2;
+           if(nums[mid] == target){
+               return mid;
+           }
+           if(nums[0] <= nums[mid]){
+               if(nums[0] <= target && target < nums[mid]){
+                   right = mid - 1;
+               }else{
+                   left = mid + 1;
+               }
+           }else{
+               if(nums[mid] < target && target <= nums[nums.length-1]){
+                   left = mid+1;
+               }else{
+                   right = mid-1;
+               }
+           }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        int target = 0;
+//        target = 3;
+        nums = new int[]{3,1};
+        target =1;
+        Search s = new Search();
+        int res = s.searchTest(nums,target);
+        System.out.println(res);
+    }
+
     public int search(int[] nums, int target) {
         int n = nums.length;
         if (n == 0) {
